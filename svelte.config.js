c7db539f42686c5cd73cb55576fb1f51c990a5cb
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2026-04-13 13:56:32 -0400

    feat: babelllll :D

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ export default {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
 	},
-	//preprocess: [vitePreprocess()]
+	// preprocess: [vitePreprocess()]
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,58 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { transformSync } from '@babel/core';
+import * as glob from 'glob';
+import * as path from 'path';
+import * as fs from 'fs';
+
+function buildExtensions() {
+  const extensionFiles = glob.sync('src/babeld-extensions/**/*.js');
+  
+  return {
+    name: 'pm-build-extensions',
+    generateBundle(_options, _bundle) {
+      extensionFiles.forEach(file => {
+        const code = transformSync(fs.readFileSync(file, 'utf-8'), {
+          assumptions: {
+            constantReexports: true,
+            constantSuper: true,
+            ignoreFunctionLength: true,
+            ignoreToPrimitiveHint: true,
+            noClassCalls: true,
+            noDocumentAll: true,
+            noNewArrows: true,
+            objectRestNoSymbols: true,
+            pureGetters: true,
+          },
+          presets: [
+            ['@babel/preset-env'],
+          ],
+          plugins: [],
+          targets: [
+            'chrome >= 70',
+            'chromeandroid >= 70',
+            'edge >= 17',
+            'firefox >= 68'
+          ],
+          sourceType: 'script',
+          filename: file,
+        }).code;
+        
+        const outputName = path.relative('src/babeld-extensions', file);
+        this.emitFile({
+          type: 'asset',
+          fileName: `extensions/${outputName}`,
+          source: `void !function() {\n  'use strict';\n${code}\n}();`,
+        });
+      });
+    },
+  };
+}
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+    buildExtensions(),
+    sveltekit(),
+  ],
 });
